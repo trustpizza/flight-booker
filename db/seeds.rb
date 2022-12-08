978e0d862c4304750b13fc64a85f071e54c0c736
trustpizza <maxelolsson@gmail.com>
2022-12-08 14:54:25 -0600

    Create 5 airports in seed file for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-airports = Airport.find_or_create_by([{code: 'SFO'}, {code: 'NYC'}, {code: 'LAX'}, {code: 'BOS'}, {code: 'ORD'}])
+Airport.find_or_create_by({code: 'SFO'})
+Airport.find_or_create_by({code: 'NYC'})
+Airport.find_or_create_by({code: 'LAX'})
+Airport.find_or_create_by({code: 'BOS'})
+Airport.find_or_create_by({code: 'ORD'})
