d2152fb045df3a7c8964c7380225b2ed52d2c266
trustpizza <maxelolsson@gmail.com>
2022-12-08 14:58:01 -0600

    Migrate database with flight model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_08_204840) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_08_205701) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "flights", force: :cascade do |t|
+    t.time "dep_time"
+    t.date "dep_date"
+    t.integer "origin_id"
+    t.integer "destination_id"
+    t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
