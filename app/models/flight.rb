5cb86d51c611a3112a5af9953a9366f242619c96
trustpizza <maxelolsson@gmail.com>
2022-12-08 15:35:37 -0600

    Airport-flight model departure and arrival relationship needs work

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,6 +4,6 @@ class Flight < ApplicationRecord
                                inverse_of: :departing_flights
   
   belongs_to :departure_airport, class_name: 'Airport',
-                                 foreign_key: :origin_id,
+                                 foreign_key: :destination_id,
                                  inverse_of: :arriving_flights
 end
