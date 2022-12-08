b8bb62c72b03ca76a9054f876aa5483d0a4a9466
trustpizza <maxelolsson@gmail.com>
2022-12-08 15:23:47 -0600

    Add Airport-Flight model relationships

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,3 +1,9 @@
 class Flight < ApplicationRecord
-  belongs_to :airport
+  belongs_to :arrival_airport, class_name: 'Airport',
+                               foreign_key: :origin_id,
+                               inverse_of: :departing_flights
+  
+  belongs_to :departure_airport, class_name: 'Airport',
+                                 foreign_key: :origin_id,
+                                 inverse_of: :arriving_flights
 end
