fd667071357a0b3ef0b949dcf9fb6d92727a4919
trustpizza <maxelolsson@gmail.com>
2022-12-08 20:10:40 -0600

    FlightsController returns flight_options with Airport destinations and origins

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,6 @@
 class FlightsController < ApplicationController
   def index
+    @flight_options = Flight.all.map { |f| [Airport.find(f.destination_id), Airport.find(f.origin_id)] }
     @flights = Flight.all
   end
 end
