ea09adcafee79ea174de48bbe49cca4bb0032dc2
trustpizza <maxelolsson@gmail.com>
2022-12-09 15:03:10 -0600

    Create #new method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,13 @@
 class BookingsController < ApplicationController
+  def new
+    @flight = Flight.includes(:origin_id, :destination_id).find(params[:flight_id])
+    @booking = @flight.bookings.build
+    params[:num_passengers].to_i.times { @booking.passengers.build }
+  end
+
+  def create
+  end
+
+  def booking_params
+  end
 end
