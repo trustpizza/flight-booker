d9f31c86b8e726a3a3299430272ec8b6f0701115
trustpizza <maxelolsson@gmail.com>
2022-12-09 17:45:54 -0600

    Database migrated to include booking as a foreign key on passengers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_09_201544) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_09_234008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,7 +42,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_09_201544) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "booking_id", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
   end
 
   add_foreign_key "bookings", "flights"
+  add_foreign_key "passengers", "bookings"
 end
