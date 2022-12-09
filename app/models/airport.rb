1e345de3d348458976747bf444236d9feb0a99c0
trustpizza <maxelolsson@gmail.com>
2022-12-09 09:37:08 -0600

    Create scope for Airport.city and fix dropdown menu

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,6 @@
 class Airport < ApplicationRecord
   has_many :departing_flights, class_name: "Flight", foreign_key: "destination_id"
   has_many :arriving_flights, class_name: "Flight", foreign_key: "origin_id"
+
+  scope :city, -> { distinct.pluck(:code)} 
 end
