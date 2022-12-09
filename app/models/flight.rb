a5a35d0cbe8eef84b2716d87b8a160f470eccee6
trustpizza <maxelolsson@gmail.com>
2022-12-09 13:11:24 -0600

    Create form radio buttons with all available departures

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: "Airport", foreign_key: "origin_id"
   belongs_to :departure_airport, class_name: "Airport", foreign_key: "destination_id"
+  
+  def display_depart_time
+    dep_time.strftime('%r')
+  end
 end
