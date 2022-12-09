9fcb29d4013ab34784338ce2d8f54468f3cd332a
trustpizza <maxelolsson@gmail.com>
2022-12-09 14:20:29 -0600

    Set up associations between Flights, bookings and passengers

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,7 +1,10 @@
 class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: "Airport", foreign_key: "origin_id"
   belongs_to :departure_airport, class_name: "Airport", foreign_key: "destination_id"
-  
+
+  has_many :bookings
+  has_many :passengers through: :bookings
+
   def display_depart_time
     dep_time.strftime('%r')
   end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,4 @@
 class Passenger < ApplicationRecord
+  belongs_to :bookings
+  has_one :flight through: :booking
 end
