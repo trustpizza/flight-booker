b767d1dd59da19d5f37212764994943f0314d27d
trustpizza <maxelolsson@gmail.com>
2022-12-09 09:34:04 -0600

    Update git for errors

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,7 @@
 class FlightsController < ApplicationController
   def index
-    @flight_names = Flight.all.map { |f| Airport.find(f.destination_id).code }
+    @flight_names = Flight.all.map { |f| [Airport.find(f.destination_id).code, f.id] }
+    @airports = Airport.all
     @flights = Flight.all
   end
 end
