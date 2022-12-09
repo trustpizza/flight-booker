4887985a1c9959f1f2e0006810009950b4fd1062
trustpizza <maxelolsson@gmail.com>
2022-12-09 14:14:52 -0600

    Create bookings model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_08_205701) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_09_201433) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_205701) do
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "flight_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -30,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_205701) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bookings", "flights"
 end
