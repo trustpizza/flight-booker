dc449b79fdcfc7bc0ced5c4c3b49d74c78e0b22b
trustpizza <maxelolsson@gmail.com>
2022-12-13 14:44:33 -0600

    Create mailer view and controller

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'notifications2email.com'
+
+  def confirmation_email
+    @passenger = params[:passenger]
+    @url = 'https://mysterious-bastion-32456.herokuapp.com/'
+    mail(to: @passenger.email, subject: 'Booking Confirmation')
+  end
 end
