7e0758fe71bc98dc794b90f3f5fde2c74d0090f9
trustpizza <maxelolsson@gmail.com>
2023-01-19 10:10:20 -0600

    Create all LAX flight durations

--- app/services/flight_generator.rb
+++ app/services/flight_generator.rb
@@ -5,17 +5,17 @@ class FlightGenerator
 
   def flight_duration
     {
-      LAX: {BOS: 330, ORD: , JFK: , ATL: , DFW: , DEN: , MCO: , PHX: , MSP: , LGA: },
-      BOS: {LAX: , ORD: , JFK: , ATL: , DFW: , DEN: , MCO: , PHX: , MSP: , LGA: },
-      ORD: {LAX: , BOS: , JFK: , ATL: , DFW: , DEN: , MCO: , PHX: , MSP: , LGA: },
-      JFK: {LAX: , BOS: , ORD: , ATL: , DFW: , DEN: , MCO: , PHX: , MSP: , LGA: },
-      ATL: {LAX: , BOS: , ORD: , JFK: , DFW: , DEN: , MCO: , PHX: , MSP: , LGA: },
-      DFW: {LAX: , BOS: , ORD: , JFK: , ATL: , DEN: , MCO: , PHX: , MSP: , LGA: },
-      DEN: {LAX: , BOS: , ORD: , JFK: , ATL: , DFW: , MCO: , PHX: , MSP: , LGA: },
-      MCO: {LAX: , BOS: , ORD: , JFK: , ATL: , DFW: , DEN: , PHX: , MSP: , LGA: },
-      PHX: {LAX: , BOS: , ORD: , JFK: , ATL: , DFW: , DEN: , MCO: , MSP: , LGA: },
-      MSP: {LAX: , BOS: , ORD: , JFK: , ATL: , DFW: , DEN: , MCO: , PHX: , LGA: },
-      LGA: {LAX: , BOS: , ORD: , JFK: , ATL: , DFW: , DEN: , MCO: , PHX: , MSP: }
+      LAX: {BOS: 330, ORD: 245, JFK: 320, ATL: 250, DFW: 180, DEN: 145, MCO: 280, PHX: 85, MSP: 215, LGA: 418},
+      BOS: {LAX: 330, ORD: , JFK: , ATL: , DFW: , DEN: , MCO: , PHX: , MSP: , LGA: },
+      ORD: {LAX: 245, BOS: , JFK: , ATL: , DFW: , DEN: , MCO: , PHX: , MSP: , LGA: },
+      JFK: {LAX: 320, BOS: , ORD: , ATL: , DFW: , DEN: , MCO: , PHX: , MSP: , LGA: },
+      ATL: {LAX: 250, BOS: , ORD: , JFK: , DFW: , DEN: , MCO: , PHX: , MSP: , LGA: },
+      DFW: {LAX: 180, BOS: , ORD: , JFK: , ATL: , DEN: , MCO: , PHX: , MSP: , LGA: },
+      DEN: {LAX: 145, BOS: , ORD: , JFK: , ATL: , DFW: , MCO: , PHX: , MSP: , LGA: },
+      MCO: {LAX: 280, BOS: , ORD: , JFK: , ATL: , DFW: , DEN: , PHX: , MSP: , LGA: },
+      PHX: {LAX: 85, BOS: , ORD: , JFK: , ATL: , DFW: , DEN: , MCO: , MSP: , LGA: },
+      MSP: {LAX: 215, BOS: , ORD: , JFK: , ATL: , DFW: , DEN: , MCO: , PHX: , LGA: },
+      LGA: {LAX: 418, BOS: , ORD: , JFK: , ATL: , DFW: , DEN: , MCO: , PHX: , MSP: }
     }
   end
 
