019027c42b93dda6ac53c8200cbe1286d74038d4
trustpizza <maxelolsson@gmail.com>
2023-01-18 19:39:49 -0600

    Clean up Flight model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,9 @@
 class Booking < ApplicationRecord
   validates :flight_id, presence: true
 
-  belongs_to :flight
-  has_many :passengers
+  has_many :seats, dependent: :destroy
+  has_many :flights, through: :seats
+  has_many :passengers, dependent: :destroy
 
   accepts_nested_attributes_for :passengers
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,11 +1,30 @@
 class Flight < ApplicationRecord
-  belongs_to :arrival_airport, class_name: "Airport", foreign_key: "origin_id"
-  belongs_to :departure_airport, class_name: "Airport", foreign_key: "destination_id"
+  belongs_to :origin_airport, class_name: "Airport", 
+                               foreign_key: "origin_id",
+                               inverse_of: :departing_flights
+  belongs_to :destination_airport, class_name: "Airport", 
+                                 foreign_key: "destination_id",
+                                 inverse_of: :arriving_flights
 
-  has_many :bookings
+  has_many :seats, dependent: :destroy
+  has_many :bookings, through: :seats
   has_many :passengers, through: :bookings
 
-  def display_depart_time
-    "#{dep_time.strftime('%r')} on #{dep_date}"
+  def flight_details
+    departure_time = 
+    arrival_time = 
+    "#{departure_time} depart from #{origin_airport.code} and arrive at #{arrival_time} at #{destination_airport.code}"
+  end
+
+  def readable_departure
+    departure_time.strftime('%l:%M %p')
+  end
+
+  def readable_arrival
+    (departure_time + (duration.to_f / 60).hours).strftime('%l:%M %p')
+  end
+
+  def readable_date
+    departure_date.strftime("%B %d, %Y")
   end
 end
