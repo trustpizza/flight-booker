6aeaefd156a0fc3017b14fc1b02df3459f5d8042
trustpizza <maxelolsson@gmail.com>
2023-01-18 19:34:03 -0600

    Add inverses to departing and arriving flights

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,6 +1,10 @@
 class Airport < ApplicationRecord
-  has_many :departing_flights, class_name: "Flight", foreign_key: "destination_id"
-  has_many :arriving_flights, class_name: "Flight", foreign_key: "origin_id"
-
-  scope :city, -> { distinct.pluck(:code)} 
+  has_many :departing_flights, class_name: "Flight", 
+                               foreign_key: "destination_id",
+                               dependent: :destroy,
+                               inverse_of: :destination_airport
+  has_many :arriving_flights, class_name: "Flight", 
+                              foreign_key: "origin_id",
+                              dependent: :destroy,
+                              inverse_of: :destination_airport
 end
